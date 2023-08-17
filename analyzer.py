d1aa9fa838037b0d046b1b71c5cba0c447eddb09
Aayush Adhikari <77236863+Aayush518@users.noreply.github.com>
2023-08-17 11:59:12 +0545

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,22 +1,50 @@
 import subprocess
 import json
 
-class CodeAnalyzer:
-    def analyze(self, file_path):
-        cmd = ['pylint', '--output-format=json', '--reports=no', file_path]
+class EnhancedCodeAnalyzer:
+    def __init__(self):
+        self.analysis_cache = {}
+    
+    def scrutinize(self, file_path):
+        if not self._is_valid_file(file_path):
+            return "Oops! This doesn't look like a wizard's spell book (Python file)."
+        
+        if file_path in self.analysis_cache:
+            return self.analysis_cache[file_path]
+        
+        commando = ['pylint', '--output-format=json', '--reports=no', file_path]
         
         try:
-            process_output = subprocess.check_output(cmd, stderr=subprocess.STDOUT).decode('utf-8')
-            parsed_output = self.parse_output(process_output)
-            return json.dumps(parsed_output, indent=2)
+            process_output = subprocess.check_output(commando, stderr=subprocess.STDOUT).decode('utf-8')
+            parsed_result = self._parse_output(process_output)
+            self.analysis_cache[file_path] = parsed_result
+            return parsed_result
             
-        except subprocess.CalledProcessError as e:
-            print(f"An error occurred during static analysis: {e.output}")
+        except subprocess.CalledProcessError as ex:
+            error_msg = f"Magical mishap! An error occurred during analysis: {ex.output}"
+            raise Exception(error_msg)
     
-    def parse_output(self, output):
+    def get_average_score(self, file_path):
+        parsed_result = self.scrutinize(file_path)
+        if 'global_note' in parsed_result:
+            return parsed_result['global_note']
+        return None
+    
+    def get_most_common_issues(self, file_path, num_issues=5):
+        parsed_result = self.scrutinize(file_path)
+        issues = parsed_result.get('messages', [])
+        if issues:
+            sorted_issues = sorted(issues, key=lambda x: x['message'], reverse=True)
+            return sorted_issues[:num_issues]
+        return []
+    
+    def _parse_output(self, output_data):
         try:
-            parsed_output = json.loads(output)
-            return parsed_output
-        except json.JSONDecodeError as e:
-            print(f"Error parsing JSON: {e}")
+            parsed_result = json.loads(output_data)
+            return parsed_result
+        except json.JSONDecodeError as json_err:
+            print(f"Oops! Error deciphering the mystical JSON: {json_err}")
             return {}
+    
+    def _is_valid_file(self, file_path):
+        return file_path.endswith('.py')
