98f4dea1abbba1d4299fb83d8a4e6b3c25ca529a
Aayush Adhikari <77236863+Aayush518@users.noreply.github.com>
2023-08-22 20:43:57 +0545

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,34 +1,35 @@
 import subprocess
 import json
+
 class EnhancedCodeAnalyzer:
     def __init__(self):
         self.analysis_cache = {}
-    
+
     def scrutinize(self, file_path):
         if not self._is_valid_file(file_path):
             return "Oops! This doesn't look like a wizard's spell book (Python file)."
-        
+
         if file_path in self.analysis_cache:
             return self.analysis_cache[file_path]
-        
-        commando = ['pylint', '--output-format=json', '--reports=no', file_path]
-        
+
+        command = ['pylint', '--output-format=json', '--reports=no', file_path]
+
         try:
-            process_output = subprocess.check_output(commando, stderr=subprocess.STDOUT).decode('utf-8')
+            process_output = subprocess.check_output(command, stderr=subprocess.STDOUT).decode('utf-8')
             parsed_result = self._parse_output(process_output)
             self.analysis_cache[file_path] = parsed_result
             return parsed_result
-            
+
         except subprocess.CalledProcessError as ex:
             error_msg = f"Magical mishap! An error occurred during analysis: {ex.output}"
             raise Exception(error_msg)
-    
+
     def get_average_score(self, file_path):
         parsed_result = self.scrutinize(file_path)
         if 'global_note' in parsed_result:
             return parsed_result['global_note']
         return None
-    
+
     def get_most_common_issues(self, file_path, num_issues=5):
         parsed_result = self.scrutinize(file_path)
         issues = parsed_result.get('messages', [])
@@ -36,7 +37,7 @@ class EnhancedCodeAnalyzer:
             sorted_issues = sorted(issues, key=lambda x: x['message'], reverse=True)
             return sorted_issues[:num_issues]
         return []
-    
+
     def _parse_output(self, output_data):
         try:
             parsed_result = json.loads(output_data)
@@ -44,6 +45,7 @@ class EnhancedCodeAnalyzer:
         except json.JSONDecodeError as json_err:
             print(f"Oops! Error deciphering the mystical JSON: {json_err}")
             return {}
-    
-    def _is_valid_file(self, file_path):
+
+    @staticmethod
+    def _is_valid_file(file_path):
         return file_path.endswith('.py')
