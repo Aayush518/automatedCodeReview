104737890eaad2b31cdbc60747cc15268b66d8e9
Aayush Adhikari <77236863+Aayush518@users.noreply.github.com>
2023-08-22 20:44:19 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import json
 from analyzer import EnhancedCodeAnalyzer
 
 def code_inspection_party():
@@ -9,16 +9,23 @@ def code_inspection_party():
         inspection_results = code_investigator.scrutinize(file_quest)
         print("Results of the Enchanted Code Inspection:")
         print(json.dumps(inspection_results, indent=2))
-        
+
         average_score = code_investigator.get_average_score(file_quest)
         if average_score is not None:
-            print(f"Average Code Quality Score: {average_score}")
-        
+            print(f"Average Code Quality Score: {average_score:.2f}")  # Format score to two decimal places
+
         common_issues = code_investigator.get_most_common_issues(file_quest)
         if common_issues:
             print("Most Common Issues:")
             for issue in common_issues:
                 print(f"- {issue['message']} ({issue['category']})")
+
+        # Additional feature: Show suggestions for improvement
+        if 'suggestions' in inspection_results:
+            print("\nSuggestions for Improvement:")
+            for suggestion in inspection_results['suggestions']:
+                print(f"- {suggestion}")
+
     except Exception as e:
         print(e)
 
